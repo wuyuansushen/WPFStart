5bc2b68d7fd3c207986390aad02fd65fca6efd54
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2020-11-07 15:21:06 +0800

    Complete WPF AboutWindow

--- obj/Debug/WPFStart_Content.g.i.cs
+++ obj/Debug/WPFStart_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/banner.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("banner.png")]
 
 

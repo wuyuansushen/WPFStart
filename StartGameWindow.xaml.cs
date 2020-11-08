c80f404a8e7681bc8957369ca51e6d8986f6411a
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2020-11-08 15:38:51 +0800

    Have added Options window

--- StartGameWindow.xaml.cs
+++ StartGameWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace WPFStart
 {
@@ -19,9 +21,64 @@ namespace WPFStart
     /// </summary>
     public partial class StartGameWindow : Window
     {
+
+        private GameOptions gameOptions;
+        private void playerNamesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (gameOptions.PlayAgainstComputer)
+                okButton.IsEnabled = (playerNamesListBox.SelectedItems.Count == 1);
+            else
+                okButton.IsEnabled = (playerNamesListBox.SelectedItems.Count == gameOptions.NumberOfPlayers);
+        }
+
         public StartGameWindow()
         {
+            if(gameOptions==null)
+            {
+                if (File.Exists("GameOptions.xml"))
+                {
+                    using (var stream=File.OpenRead("GameOptions.xml"))
+                    {
+                        var serializer = new XmlSerializer(typeof(GameOptions));
+                        gameOptions = serializer.Deserialize(stream) as GameOptions;
+                    }
+                }
+                else
+                {
+                    gameOptions = new GameOptions();
+                }
+            }
+            DataContext = gameOptions;
             InitializeComponent();
+            if (gameOptions.PlayAgainstComputer)
+                playerNamesListBox.SelectionMode = SelectionMode.Single;
+            else
+                playerNamesListBox.SelectionMode = SelectionMode.Extended;
+        }
+
+        private void addNewPlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(newPlayerTextBox.Text))
+                gameOptions.AddPlayer(newPlayerTextBox.Text);
+            newPlayerTextBox.Text = string.Empty;
+        }
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(string item in playerNamesListBox.SelectedItems)
+            {
+                gameOptions.SelectedPlayers.Add(item);
+            }
+            using (var stream = File.Open("GameOptions.xml",FileMode.Create))//Create File in current folder
+            {
+                var serializer = new XmlSerializer(typeof(GameOptions));
+                serializer.Serialize(stream, gameOptions);
+            }
+            Close();
+        }
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            gameOptions = null;
+            Close();
         }
     }
 }
